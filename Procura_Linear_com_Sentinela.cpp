b0f0745ebf3e6994e30cc0f33c2ef790937f374c
Raicley Santana <raicleysantana1@gmail.com>
2020-09-16 00:04:29 -0400

    inial commit

---
+++ Procura_Linear_com_Sentinela.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+struct fichaCarro{
+ 	char PILOTO;
+ 	char CARRO;
+	float TEMPO;
+ 	int CLASS;
+}
+
+struct Rally{
+		CARROS;
+	int TOTAL;
+};
+
+int main(int argc, char** argv){
+int proc_sent;
+int i;
+}
+
+
+void proc_piloto(lista,elem){
+	int i = 1;
+	
+	list.CARROS[list.TOTAL + 1].PILOTO = elem;
+	
+	while(list.CARROS[i].PILOTO !=== elem){
+		i = i+1;
+	}
+	
+	if(i==list.TOTAL+1){
+		i = 0;
+	}else{
+		
+	}
+	
+}
